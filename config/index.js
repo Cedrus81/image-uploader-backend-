61b37cd53491ec10ded589fb12e160e875c57991
Cedrus81 <erez.eitan.97@gmail.com>
2023-02-25 21:22:05 +0200

    some fixes before another run build

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ var config
 if (process.env.NODE_ENV === 'production') {
   config = require('./prod')
 } else {
+  console.log(process.env.NODE_ENV)
   config = require('./dev')
 }
 config.isGuestMode = true
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://erez:OQ2fWI7udB3PDnOU@cluster0.9ggajqn.mongodb.net/?retryWrites=true&w=majority',
+  // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
   dbName: 'image_db'
 }
