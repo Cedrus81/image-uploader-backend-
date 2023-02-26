4bcac093506f9a3e27ca1c9b23e4f90211c1eead
Cedrus81 <erez.eitan.97@gmail.com>
2023-02-26 10:38:07 +0200

    security

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbURL: 'mongodb+srv://erez:OQ2fWI7udB3PDnOU@cluster0.9ggajqn.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: process.env.DB_URL,
   // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
   dbName: 'image_db'
 }
