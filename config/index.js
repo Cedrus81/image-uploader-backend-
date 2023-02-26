13cbeb5506c58fd8319041e5d0e68b85936334bc
Cedrus81 <erez.eitan.97@gmail.com>
2023-02-26 12:31:20 +0200

    smallfix

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 var config
 
-if (process.env.NODE_ENV === 'production') {
-  config = require('./prod')
-} else {
-  console.log(process.env.NODE_ENV)
-  config = require('./dev')
-}
+// if (process.env.NODE_ENV === 'production') {
+config = require('./prod')
+// } else {
+// console.log(process.env.NODE_ENV)
+// config = require('./dev')
+// }
 config.isGuestMode = true
 
 module.exports = config
--- server.js
+++ server.js
@@ -12,15 +12,15 @@ app.use(cookieParser())
 app.use(express.json())
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
-} else {
-    const corsOptions = {
-        origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3030', 'http://localhost:3030'],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.resolve(__dirname, 'public')))
+// } else {
+//     const corsOptions = {
+//         origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3030', 'http://localhost:3030'],
+//         credentials: true
+//     }
+//     app.use(cors(corsOptions))
+// }
 
 // const authRoutes = require('./api/auth/auth.routes')
 // const userRoutes = require('./api/user/user.routes')
