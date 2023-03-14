92e3f4bcef7145cc0dfed0cc59df094e609d0dce
Cedrus81 <erez.eitan.97@gmail.com>
2023-03-14 14:39:47 +0200

    added in cloudinary api key for delete requests

--- services/cloudinary.service.js
+++ services/cloudinary.service.js
@@ -1,9 +1,11 @@
 const cloudinary = require('cloudinary').v2;
+cloudinary.config({ api_key: process.env.CLOUDINARY_API_KEY })
+
 const CLOUDINARY_FOLDER = 'image-uploader/'
 
 async function removeFromCloudinary(url) {
     const public_id = url.split(CLOUDINARY_FOLDER)[1].split('.')[0]
-    const res = await cloudinary.uploader.destroy(CLOUDINARY_FOLDER + public_id,).then(result => result.result)
+    const res = await cloudinary.uploader.destroy(CLOUDINARY_FOLDER + public_id).then(result => result.result)
     if (res === 'not found') throw new Error('image not found in cloudinary')
 }
 
