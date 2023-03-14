74a55870f4fd4b761e9d9b575e392d90bdcebfdd
Cedrus81 <erez.eitan.97@gmail.com>
2023-03-14 14:51:06 +0200

    same w api secret

--- services/cloudinary.service.js
+++ services/cloudinary.service.js
@@ -1,5 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-cloudinary.config({ api_key: process.env.CLOUDINARY_API_KEY })
+cloudinary.config({ api_key: process.env.CLOUDINARY_API_KEY, api_secret: process.env.CLOUDINARY_SECRET, cloud_name: process.env.CLOUDINARY_NAME })
 
 const CLOUDINARY_FOLDER = 'image-uploader/'
 
